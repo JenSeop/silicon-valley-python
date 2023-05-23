357eac7b79e79d5a3cc4f8de48a21aaaa899e438
jungskim <dostwoman@gmail.com>
2023-05-23 16:00:45 +0900

    [23.05.23] Radomization

---
+++ 11.Randomization/ex01.py
@@ -0,0 +1,10 @@
+# random module
+import random
+
+# random float
+data = random.random()
+print(f"float random : {data}")
+
+# random int 1~100
+data = random.randint(1,100)
+print(f"int random : {data}")
---
+++ 11.Randomization/ex02.py
@@ -0,0 +1,3 @@
+import ex02_module
+
+print(ex02_module.data01, ex02_module.data02)
