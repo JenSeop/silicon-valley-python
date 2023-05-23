f47bb2d8d1338ecbd79ba8b65c98148a1484cb36
jungskim <dostwoman@gmail.com>
2023-05-23 15:37:17 +0900

    [23.05.23] upload M Condition ex

---
+++ 09.Multiple Condition ex/ex01.py
@@ -0,0 +1,8 @@
+money = (int)(input("How many do u have money? : "))
+print(f"Ur money is {money}$, isn't it?")
+if money > 10:
+    print(f"{money} is bigger than 10$")
+if money > 20:
+    print(f"{money} is bigger than 20$")
+else:
+    print(f"Do u have {money}$? equal my bank count {money}$")
