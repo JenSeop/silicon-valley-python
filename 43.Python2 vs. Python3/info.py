a3f6fc9fc8b6c7451be7bf741da6f7360889c8c4
jungskim <dostwoman@gmail.com>
2023-06-01 20:31:59 +0900

    [23.06.01] update Python2 vs. Python3

--- 43.Python2 vs. Python3/info.py
+++ 43.Python2 vs. Python3/info.py
@@ -1,75 +1,75 @@
-History
+# History
 
-Python 2
-Introduced in 2000
-make programming simple and easy to lean for the common mass
+# Python 2
+# Introduced in 2000
+# make programming simple and easy to lean for the common mass
 
-Python 3
-Introduced in 2008
-redundancy - writing repetitive code or writing the same piece of code again
-and again - should be removed from coding
+# Python 3
+# Introduced in 2008
+# redundancy - writing repetitive code or writing the same piece of code again
+# and again - should be removed from coding
 
-Syntax
+# Syntax
 
-Python 2
-print "Hello World" # statement
+# Python 2
+# print "Hello World" # statement
 
-Python 3
-print("Hello World") # function
+# Python 3
+# print("Hello World") # function
 
-Storage of Strings
+# Storage of Strings
 
-Python 2
-ASCII: a method of encoding English characters with an assigned number
-Do not have a enough symbols due to 7 bits
+# Python 2
+# ASCII: a method of encoding English characters with an assigned number
+# Do not have a enough symbols due to 7 bits
 
-Python 3
-Unicode: the practical UTF-8 (Unicode Transformation Format-8-bit)
-support foreign languages and other widely used symbols and emojis.
+# Python 3
+# Unicode: the practical UTF-8 (Unicode Transformation Format-8-bit)
+# support foreign languages and other widely used symbols and emojis.
 
-Libraries
+# Libraries
 
-Python 2
-Not forward compatible, Hard to porting from Python2 to Python3 is hell
-No longer supported by the year 2020
+# Python 2
+# Not forward compatible, Hard to porting from Python2 to Python3 is hell
+# No longer supported by the year 2020
 
-Python 3
-Backward-incompatible. Always gear toward future improvements
-A lot of libraries are specific for Python3
+# Python 3
+# Backward-incompatible. Always gear toward future improvements
+# A lot of libraries are specific for Python3
 
-Division of Integers
+# Division of Integers
 
-Python 2
-Round down for the integer division ( 9 divided by 4 is 2 )
+# Python 2
+# Round down for the integer division ( 9 divided by 4 is 2 )
 
-Python 3
-Exact number ( 9 divided by 4 is 2.25 )
+# Python 3
+# Exact number ( 9 divided by 4 is 2.25 )
 
-Variable leakage
+# Variable leakage
 
-Python 2
-The values of global variables do change in Python 2 if they are used inside
-a for-loop.
+# Python 2
+# The values of global variables do change in Python 2 if they are used inside
+# a for-loop.
 
-i = 0
-a = [i for i in range(3)]
-print(i) # Ouputs 2
+# i = 0
+# a = [i for i in range(3)]
+# print(i) # Ouputs 2
 
-Python 3
-The value of variables never changes in Python 3.
+# Python 3
+# The value of variables never changes in Python 3.
 
-i = 0
-a = [i for i in range(3)]
-print(i) # Outputs 0
+# i = 0
+# a = [i for i in range(3)]
+# print(i) # Outputs 0
 
-Use case
+# Use case
 
-Python 2
-Used by DevOps engineer
+# Python 2
+# Used by DevOps engineer
 
-Python 3
-Used by Software Engineer, Data Science and etc
+# Python 3
+# Used by Software Engineer, Data Science and etc
 
-Trend
+# Trend
 
-From 2015~ common used Python 3
+# From 2015~ common used Python 3
