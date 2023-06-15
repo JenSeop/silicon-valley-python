457a590507be00fc8c53fbcf38b1e8b86b41d0d7
JenSeop <dostwoman@gmail.com>
2023-06-15 18:41:54 +0900

    [23.06.15] update Class method

--- 58.Class method/ex01.py
+++ 58.Class method/ex01.py
@@ -24,5 +24,8 @@ class Car:
 # print(Car.hyundai())
 print(Car.ferrari())
 
-# class methods take a cls parameter that points to the class—and not the object instance—when the method is called
-# Because the class method only has access to this cls argument, it can’t modify object instance state. That would require access to self. However, class methods can still modify class state that applies across all instances of the class.
+# class methods take a cls parameter that points to the
+# class—and not the object instance—when the method is called
+# Because the class method only has access to this cls argument,
+# it can’t modify object instance state. That would require access to self.
+# However, class methods can still modify class state that applies across all instances of the class.
